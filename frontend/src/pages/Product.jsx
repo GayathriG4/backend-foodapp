2fe3455bd5da758d9b18ece8cb6d7139e34245d6
Gayathri Arumugam <gayathriarumugam284@gmail.com>
2025-01-20 22:25:26 +0000

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,28 +1,38 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { all_products } from "../assets/data";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
   const { productId } = useParams();
-  // console.log("productId: ", productId)
-
+  const { all_products, url } = useContext(ShopContext);
+  
   const product = all_products.find((e) => e._id === productId);
+
   if (!product) {
     return <div className="h1 pt-28">Product not Found</div>;
   }
 
   return (
     <section className="max-padd-container py-16 xl:py-28">
-      {/* title */}
+      {/* Title */}
       <div className="py-10 max-w-[622px] pb-20">
-        <h3 className="h3 uppercase ">Your Chosen Flavors ?</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quidem,
-          pariatur omnis, iste libero fugit aliquam recusandae velit, tenetur
-          provident non!
-        </p>
+        <h3 className="h3 uppercase ">Your Chosen Flavors</h3>
+        <p>{product.description}</p>
       </div>
-      {/* container */}
+
+      {/* Product Details */}
+      <div className="flex flex-col lg:flex-row gap-10 items-center">
+        <img
+          src={`${url}/images/${product.image}`}
+          alt={product.name}
+          className="rounded-lg shadow-md w-full max-w-[400px]"
+        />
+        <div>
+          <h3 className="h3">{product.name}</h3>
+          <p className="medium-16 text-gray-50">Price: ${product.price}</p>
+          <p className="medium-16 text-gray-50">Category: {product.category}</p>
+        </div>
+      </div>
     </section>
   );
 };
