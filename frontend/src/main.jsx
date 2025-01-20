6eb26097b39ca864527b15ac73721f7a791977a9
Gayathri Arumugam <gayathriarumugam284@gmail.com>
2025-01-20 22:22:19 +0000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-import ShopContextProvider from "./context/ShopContext.jsx";
+import ShopContextProvider from "./context/ShopContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
